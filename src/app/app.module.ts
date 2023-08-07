05e5dbe544874e92d28a39fd6589eb3e64682dd1
Ghiftgky <aguantecordoba@hotmail.com>
2023-08-06 21:31:41 -0300

    final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { CuerpoComponent } from './componentes/cuerpo/cuerpo.component';
+import { PieComponent } from './componentes/pie/pie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    CuerpoComponent,
+    PieComponent
   ],
   imports: [
     BrowserModule
